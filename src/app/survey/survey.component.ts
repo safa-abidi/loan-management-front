aaad3022f774926bc4d3472d9aef9e0bb3797421
safa-abidi <safa.abidi99@gmail.com>
2023-04-20 01:39:46 +0100

    initial ui

---
+++ src/app/survey/survey.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-survey',
+  templateUrl: './survey.component.html',
+  styleUrls: ['./survey.component.css'],
+})
+export class SurveyComponent {
+}
